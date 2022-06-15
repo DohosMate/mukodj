9d32c216d5fcef0260787a3179d2aa1a223e802e
DohosMate <dohosmail@gmail.com>
2022-06-15 16:40:32 +0200

    initial welcome

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
-   
+import './stlye.css';
+import logo from "./hamarosan_nyitunk.jpg";
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        Comming Soon
-        <h5>Created By: Máté Dohos 2022</h5>
-      </header>
+    <div>
+      <p className="welcome">Üdvözlünk a _<p className='foldieden_title'>Földi Éden_</p>Webáruházában!</p>
+    <div className='picture_container'>
+      <img src={logo} width={500} height={500} alt="" />
     </div>
-  );
+    <footer>
+      <p className="line anim-typewriter">Hamarosan nyitunk: 2022.09.01</p>
+      <hr />
+      <h3>Created by 3BM Corporation</h3>
+    </footer>
+    </div>
+    );
 }
 
 export default App;
- 
